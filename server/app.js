b5b0c27d9ad28732b8cf3e18d2631019dd4e2238
Wen Xia <wen.xia@wework.com>
2021-02-04 18:09:04 +0800

    t

--- server/app.js
+++ server/app.js
@@ -1,9 +1,48 @@
+const fs = require("fs");
 const Koa = require("koa");
+
 const app = new Koa();
+
+const Json2csvParser = require("json2csv").Parser;
+const fields = ["car", "price", "color", "for"];
+
+// let myData = fs.readFileSync("./data.json");
+
+const json2csvParser = new Json2csvParser({ fields });
+
+const csv = json2csvParser.parse([
+  {
+    car: "Audi",
+    price: 40000,
+    color: "blue",
+    for: "",
+    sss: "",
+  },
+  {
+    car: "BMW",
+    price: 35000,
+    color: "black",
+    for: "",
+  },
+  {
+    car: "Porsche",
+    price: 60000,
+    color: "green",
+    for: "",
+  },
+]);
+
+fs.writeFile("./data.csv", csv, function (err) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("The file was saved!");
+});
 
 // response
 app.use((ctx) => {
-  ctx.body = "Hello Koa";
+  ctx.body = "excel";
 });
 
 app.listen(3000, () => {
